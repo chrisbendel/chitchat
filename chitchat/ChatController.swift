8cd3b9e74f57209447ed456fd4c067c99f3dad16
Chris Bendel <chrissbendel@gmail.coml>
2018-04-03 14:49:27 -0400

    layout

--- chitchat/ChatController.swift
+++ chitchat/ChatController.swift
@@ -1,7 +1,8 @@
 import UIKit
-class ChatController: UIViewController {
+class ChatController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var message: UITextField!
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -10,7 +11,7 @@ class ChatController: UIViewController {
                 print(error ?? "Unknown error")
                 return
             }
-            print(responseObject)
+            let messages = responseObject["messages"]
         }
     }
 
@@ -18,7 +19,19 @@ class ChatController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // your cell coding
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // cell selected code here
+    }
 
     @IBAction func sendMessage(_ sender: Any) {
     }
